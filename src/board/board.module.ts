b64ac0a5ec901d73f6e77f052703aea113e913c7
jinjinchoi <https://github.com/jinjinchoi>
2024-07-19 16:53:37 +0900

    [feat] 데이터베이스 생성까지 추가

---
+++ src/board/board.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { BoardController } from './board.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Board } from './entities/board.entity';
+
+@Module({
+  imports: [SequelizeModule.forFeature([Board])],
+  controllers: [BoardController],
+  providers: [BoardService],
+})
+export class BoardModule {}
