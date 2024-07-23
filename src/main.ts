6009d236f489df5f434cc67cffe1e1141d2cafea
jinjinchoi <https://github.com/jinjinchoi>
2024-07-23 14:37:12 +0900

    [feat] cors 설정

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ import { setupSwagger } from './swagget';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 스웨거 설정
   setupSwagger(app);
+
+  // cors 설정
+  app.enableCors({
+    origin: "http://localhost:3000", // 나중에 수정
+    methods: 'GET, HEAD, PATCH, POST, DELETE'
+  })  
 
   await app.listen(3000);
 }
